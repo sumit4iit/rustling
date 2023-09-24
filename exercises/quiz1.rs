155dda055c1ce7792bd85a3271d13677a0459ed7
Sumit Agrawal <sumitsa@amazon.com>
2023-09-24 16:49:44 +0530

    save progress

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,13 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
 // fn calculate_price_of_apples {
+
+fn calculate_price_of_apples(q: i32) -> i32 {
+    if q > 40 {1 * q} else { 2 * q }
+}
 
 // Don't modify this function!
 #[test]
